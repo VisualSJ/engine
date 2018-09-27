4b3c70b7158e6b43aea3f46d276c684b4e601091
Yanna <yuanyuan1497@foxmail.com>
2018-09-27 15:27:46 +0800

    Fix UI component (#147)
    * 修复checkbox样式问题
    
    * 移除ui-section中header右边距，修复color组件focused效果
    
    * 添加ui-prop添加移除测试例子,修复拖动改变num-input值时，离开ui-prop的焦点问题
    
    * 添加ui-prop控制label最大最小宽度，更改ui-preview样式
    
    * 删除ui-input、ui-num-input组件内的默认宽度，添加外部对input宽度的控制
    
    * 删除其他组件的默认宽度，以及测试预览代码使用更改

--- builtin/ui-preview/source/components/ui-prop.ts
+++ builtin/ui-preview/source/components/ui-prop.ts
@@ -6,9 +6,52 @@ import { join } from 'path';
 export const template = readFileSync(join(__dirname, '../../static/template/components/ui-prop.html'), 'utf8');
 export function data() {
     return {
+        eventName: '',
+        newItems: [], // 新增元素
+        itemHtmls: ''
     };
 }
 
 export const methods = {
+    /**
+     * 测试元素的confirm监听事件
+     * @param event
+     */
+    confirmValue(event: Event) {
+        // @ts-ignore
+        this.eventName = 'confirm';
+        // @ts-ignore
+        this.value = event.target.value;
+    },
 
+    /**
+     * 测试元素的change监听事件
+     * @param event
+     */
+    changeValue(event: Event) {
+        // @ts-ignore
+        this.eventName = 'change';
+        // @ts-ignore
+        this.value = event.target.value;
+    },
+
+    /**
+     * 测试添加元素后事件等等是否会出现异常
+     */
+    addItem() {
+        // @ts-ignore
+        this.newItems.push(`<ui-prop label="string" type="string" value="kkkkk"> </ui-prop>`);
+        // @ts-ignore
+        this.itemHtmls = this.newItems.toString().replace(/,/g, '');
+    },
+
+    /**
+     * 测试移除元素后事件等等是否会出现异常
+     */
+    removeItem() {
+        // @ts-ignore
+        this.newItems.pop();
+        // @ts-ignore
+        this.itemHtmls = this.newItems.toString().replace(/,/g, '');
+    }
 };
