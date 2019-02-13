1059016c452e924bba68f54efe9d91fc873f8150
SijieWang <itharbors@outlook.com>
2019-02-13 14:41:06 +0800

    Hack: some of the cc.require functionality is supported (#716)

--- builtin/scene/static/script/3d/manager/startup/polyfills/index.js
+++ builtin/scene/static/script/3d/manager/startup/polyfills/index.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const { basename } = require('path');
+
 function editor() {
     // HACK: 设置 CC_EDITOR 标记，引擎加载的时候会使用标记进行部分判断
     window.CC_EDITOR = true;
@@ -10,6 +12,27 @@ function editor() {
 function engine() {
     // HACK: 模拟 cc.engine，防止引擎运行时报错
     require('./engine');
+
+    let loadingProjectScripts = 0;
+    cc.require = function(request, originRequire) {
+        originRequire = originRequire || require;
+        loadingProjectScripts ++;
+
+        let m;
+        try {
+            const name = basename(request);
+
+            m = cc.js.getClassByName(name);
+            if (!m) {
+                m = originRequire(request);
+            }
+        } catch (err) {
+            console.error(`load script [${request}] failed : ${err.stack}`);
+        }
+
+        loadingProjectScripts --;
+        return m;
+    };
 }
 
 exports.editor = editor;
