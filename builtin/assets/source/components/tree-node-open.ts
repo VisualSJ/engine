45fc0af6639917174c828abb78a73b81645e4159
Arsen Huang <arsen2010@126.com>
2019-02-28 14:09:13 +0800

    Double-click the resources in the assets palette (#797)

--- builtin/assets/source/components/tree-node-open.ts
+++ builtin/assets/source/components/tree-node-open.ts
@@ -1,9 +1,10 @@
 'use strict';
-
+import { shell } from 'electron';
 /**
  * 打开一个 asset
  * 根据文件的后缀处理
  */
+
 exports['.scene'] = (asset: ItreeAsset) => {
     Editor.Ipc.sendToPackage('scene', 'open-scene', asset.uuid);
 };
@@ -19,18 +20,50 @@ exports['.fire'] = async (asset: ItreeAsset) => {
     Editor.Ipc.sendToPackage('scene', 'open-scene', asset.uuid);
 };
 
-exports['.js'] = (asset: ItreeAsset) => {
-    openFile(asset.uuid);
-};
+exports['.js'] = openFile;
+exports['.ts'] = openFile;
+exports['.coffee'] = openFile;
+exports['.md'] = openFile;
+exports['.json'] = openFile;
 
-exports['.ts'] = (asset: ItreeAsset) => {
-    openFile(asset.uuid);
-};
+exports['.png'] = openFile;
+exports['.apng'] = openFile;
+exports['.jpg'] = openFile;
+exports['.jpge'] = openFile;
+exports['.gif'] = openFile;
 
-exports['.json'] = (asset: ItreeAsset) => {
-    openFile(asset.uuid);
-};
+function openFile(asset: ItreeAsset) {
+    const openRule: any = {
+        text: {
+            cmd: '',
+            types: [
+                '.js',
+                '.ts',
+                '.coffee',
+                '.md',
+                '.json',
+            ],
+        },
+        image: {
+            cmd: '',
+            types: [
+                '.png',
+                '.jpg',
+                '.jpge',
+                '.gif',
+                '.apng',
+            ],
+        },
+    };
 
-function openFile(uuid: string) {
-    // TODO
+    const rules = Object.keys(openRule);
+    for (const rule of rules) {
+        const {cmd, types} = openRule[rule];
+        if (cmd !== '' && types.includes(asset.fileExt)) {
+            return; // TODO 暂时没有自定义操作
+        }
+    }
+
+    // 最后由系统程序尝试打开一次
+    shell.openItem(asset.file);
 }
